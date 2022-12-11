985c9017676367f2429c9d0db76c1bda71f19eae
Sergio Gimeno <sergiogmn@gmail.com>
2022-12-11 19:39:26 +0100

    Errors arreglats

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -134,40 +134,4 @@ class EquipsController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/membre/inserir", name="inserir_membre")
-     */
-    public function inserirMembre(ManagerRegistry $doctrine)
-    {
-        $repositori = $doctrine->getRepository(Equip::class);
-        $equip = $repositori->find(1);
-
-        $entityManager = $doctrine->getManager();
-        $success = true;
-        $error = null;
-
-        $membre = new Membre();
-        $membre->setNom("Sarah");
-        $membre->setCognoms("Connor");
-        $membre->setEmail("sarahconnor@skynet.com");
-        $membre->setImatgePerfil("sarahconnor.jpg");
-        $membre->setDataNaixement(new \DateTime("1963-11-29"));
-        $membre->setNota(9.7);
-        $membre->setEquip($equip);
-
-        $entityManager->persist($membre);
-        try {
-            $entityManager->flush();
-        } catch (Exception $e) {
-            $error = $e->getMessage();
-            $success = false;
-        }
-
-        return $this->render('insert_membre.html.twig', array(
-            'success' => $success,
-            'error' => $error,
-            'membre' => $membre
-        ));
-    }
-
 }
